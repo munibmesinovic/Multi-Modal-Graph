37914e2e4b021f173fa35e1db7f10afc5ee49f3b
Munib Mesinovic <102674585+munibmesinovic@users.noreply.github.com>
2025-05-08 15:45:22 +0100

    Update 05_vitals.py

--- 05_vitals.py
+++ 05_vitals.py
@@ -30,7 +30,6 @@ time_series_vitals.set_index(['CSN', 'Time'], inplace=True)
 # Time shift so that the starting point for each sample is time = 0
 time_series_vitals.reset_index(level=1, inplace=True)
 minimum_shifts = time_series_vitals.groupby('CSN')['Time'].min()
-# minimum_shifts = timeseries_summary.time.min(level=0)
 time_series_vitals = time_series_vitals.merge(minimum_shifts, left_index=True, right_index=True)
 time_series_vitals['Time'] = time_series_vitals['Time_x'] - time_series_vitals['Time_y']
 time_series_vitals.drop(columns=['Time_x', 'Time_y'], inplace=True)
@@ -38,21 +37,6 @@ time_series_vitals.set_index(['Time'], append=True, inplace=True)
 
 # Access the 'Result_time' column (which is now a timedelta)
 result_time_series = time_series_vitals.index.get_level_values('Time')  # Get Result_time from the index
-
-# Calculate total hours from the TimedeltaIndex
-result_time_hours = result_time_series.total_seconds() / 3600  # Get total seconds and convert to hours
-
-# For plotting the distribution in days:
-plt.figure(figsize=(10, 6))
-plt.hist(result_time_hours, bins=30)
-
-plt.xlabel('Result Time (Hours)')
-plt.ylabel('Frequency')
-plt.title('Distribution of Result Time')
-plt.show()
-
-# For descriptive statistics:
-print(pd.Series(result_time_hours).describe())
 
 time_series_vitals = time_series_vitals.groupby(level=[0, 1]).mean()
 
@@ -72,8 +56,6 @@ time_series_vitals.Time = pd.to_timedelta(time_series_vitals.Time, errors='coerc
 time_series_vitals.update(time_series_vitals.groupby(level=0).ffill())
 time_series_vitals.update(time_series_vitals.groupby(level=0).bfill())
 
-len(pd.unique(time_series_vitals.index.get_level_values(0)))
-
 time_series_vitals.reset_index(inplace=True)
 time_series_vitals.set_index(['CSN', 'Time'], inplace=True)
 
@@ -91,9 +73,6 @@ result_time_series = time_series_vitals.index.get_level_values('Time')  # Get Re
 # Calculate total hours from the TimedeltaIndex
 result_time_hours = result_time_series.total_seconds() / 3600  # Get total seconds and convert to hours
 
-# For descriptive statistics:
-print(pd.Series(result_time_hours).describe())
-
 time_series_labs.index = time_series_labs.index.rename('Time', level='Result_time')
 
 time_series = time_series_vitals.merge(time_series_labs, left_index=True, right_index=True)
@@ -110,33 +89,8 @@ final = time_series.merge(visits, left_index=True, right_index=True)
 missing_samples = final[final.isnull().any(axis=1)].index.get_level_values(0).tolist()
 final.drop(missing_samples, level=0, axis=0, inplace=True)
 
-len(pd.unique(final.index.get_level_values(0)))
-
-# Check for NaNs
-print("Missing values:", final['Age'].isna().sum())
-
-# Check for non-numeric values
-non_numeric = final[~final['Age'].astype(str).str.replace('.', '', 1).str.isdigit()]
-print("Non-numeric values:\n", non_numeric)
-
 # Convert to numeric, forcing errors to NaN
 final['Age'] = pd.to_numeric(final['Age'], errors='coerce')
 
 # Check for negative or unrealistic values
 invalid_ages = final[(final['Age'] < 0) | (final['Age'] > 120)]
-print("Invalid ages:\n", invalid_ages)
-
-# # Group by the first level index ('patient') and get the size of each group
-# group_sizes = final.groupby(level=0).size()
-
-# # Filter the group sizes to get patients with more than 24 entries
-# patients_to_keep = group_sizes[group_sizes >= 24].index
-
-# # Filter the original DataFrame to keep only the selected patients
-# final = final.loc[patients_to_keep]
-
-len(pd.unique(final.index.get_level_values(0)))
-
-len(final.index.get_level_values(0))
-
-final.index.get_level_values(0)
