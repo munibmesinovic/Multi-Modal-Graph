70a4135943fcf11a88565527903ed337e2d64af1
Munib Mesinovic <102674585+munibmesinovic@users.noreply.github.com>
2025-05-08 15:44:30 +0100

    Update 04_labs.py

--- 04_labs.py
+++ 04_labs.py
@@ -40,44 +40,6 @@ time_series_labs['Result_time'] = time_series_labs['Result_time_x'] - time_serie
 time_series_labs.drop(columns=['Result_time_x', 'Result_time_y'], inplace=True)
 time_series_labs.set_index(['Result_time'], append=True, inplace=True)
 
-# Access the 'Result_time' column (which is now a timedelta)
-result_time_series = time_series_labs.index.get_level_values('Result_time')  # Get Result_time from the index
-
-# Calculate total hours from the TimedeltaIndex
-result_time_hours = result_time_series.total_seconds() / 3600  # Get total seconds and convert to hours
-
-# For plotting the distribution in days:
-plt.figure(figsize=(10, 6))
-plt.hist(result_time_hours, bins=30)
-
-plt.xlabel('Result Time (Hours)')
-plt.ylabel('Frequency')
-plt.title('Distribution of Result Time')
-plt.show()
-
-# For descriptive statistics:
-print(pd.Series(result_time_hours).describe())
-
-# # take the mean of any duplicate index entries for unstacking
-# time_series_labs = time_series_labs.groupby(level=[0, 1]).mean()
-
-# # Round up the time-stamps to the next day
-# time_series_labs.reset_index(level=1, inplace=True)
-# time_series_labs['Result_time'] = pd.to_datetime(time_series_labs['Result_time'], errors='coerce')
-
-# # Time shift so that the starting point for each sample is time = 0
-# minimum_shifts = time_series_labs.groupby('CSN')['Result_time'].min()
-# time_series_labs = time_series_labs.merge(minimum_shifts, left_index=True, right_index=True)
-# time_series_labs['Result_time'] = time_series_labs['Result_time_x'] - time_series_labs['Result_time_y']
-# time_series_labs.drop(columns=['Result_time_x', 'Result_time_y'], inplace=True)
-# time_series_labs.set_index(['Result_time'], append=True, inplace=True)
-
-# # # Calculate timedelta from a reference date (e.g., minimum date)
-# # reference_date = time_series_labs['Result_time'].min()  # Or a specific date if needed
-# # time_series_labs['Result_time'] = time_series_labs['Result_time'] - reference_date
-
-#Proceed with other operations like ceil, set_index, etc.
-
 time_series_labs = time_series_labs.groupby(level=[0, 1]).mean()
 
 # Round up the time-stamps to the next hour
@@ -96,8 +58,6 @@ time_series_labs.Result_time = pd.to_timedelta(time_series_labs.Result_time, err
 time_series_labs.update(time_series_labs.groupby(level=0).ffill())
 time_series_labs.update(time_series_labs.groupby(level=0).bfill())
 
-len(pd.unique(time_series_labs.index.get_level_values(0)))
-
 time_series_labs.reset_index(inplace=True)
 time_series_labs.set_index(['CSN', 'Result_time'], inplace=True)
 
@@ -110,6 +70,3 @@ result_time_series = time_series_labs.index.get_level_values('Result_time')  # G
 
 # Calculate total hours from the TimedeltaIndex
 result_time_hours = result_time_series.total_seconds() / 3600  # Get total seconds and convert to hours
-
-# For descriptive statistics:
-print(pd.Series(result_time_hours).describe())
